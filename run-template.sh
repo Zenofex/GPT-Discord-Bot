8aaa74aa8dc8e05fba1356a78872c081a44b508e
Zenofex <zenofex@exploitee.rs>
2023-12-06 05:52:55 -0600

    update requirements.txt to add specific open-ai version, stability-sdk, and add GUILD_ID to run template.

--- run-template.sh
+++ run-template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t gptbot .
-docker run -d --restart always -v $(pwd)/data:/data/  -e STABILITY_KEY=KEYHERE -e SUMMARY_FILE=summary.sqlite3 -e BOT_ROLE=admin -e DISCORD_TOKEN=TOKENHERE -e OPENAI_API_KEY=APIHERE gptbot
+docker run -d --restart always -v $(pwd)/data:/data/  -e STABILITY_KEY=KEY_HERE -e SUMMARY_FILE=summary.sqlite3 -e BOT_ROLE=admin -e DISCORD_TOKEN=TOKEN_HERE -e OPENAI_API_KEY=APIHERE -e GUILD_ID=GUILD_ID_HERE  gptbot

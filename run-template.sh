44ac7da3ac2c2938f80c9ca64ebb0f5efff6e495
Zenofex <zenofex@exploitee.rs>
2024-04-10 16:08:13 -0500

    Added placeholder for anthropic key.

--- run-template.sh
+++ run-template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t gptbot .
-docker run -d --restart always -v $(pwd)/data:/data/  -e STABILITY_KEY=KEY_HERE -e SUMMARY_FILE=summary.sqlite3 -e BOT_ROLE=admin -e DISCORD_TOKEN=TOKEN_HERE -e OPENAI_API_KEY=APIHERE -e GUILD_ID=GUILD_ID_HERE  gptbot
+docker run -d --restart always -v $(pwd)/data:/data/ -e ANTHROPIC_TOKEN=KEY_HERE -e STABILITY_KEY=KEY_HERE -e SUMMARY_FILE=summary.sqlite3 -e BOT_ROLE=admin -e DISCORD_TOKEN=TOKEN_HERE -e OPENAI_API_KEY=APIHERE -e GUILD_ID=GUILD_ID_HERE  gptbot
